d149915dbef21a1e8fc3bc6b8101dc475324861f
Yaroslav Glivenko <yapridumalemail@gmail.com>
2025-11-09 17:30:00 +0300

    complete second task

--- JS-Koans/topics/2 Массивы и объекты/about_objects.js
+++ JS-Koans/topics/2 Массивы и объекты/about_objects.js
@@ -1,65 +1,64 @@
-
-describe("About Objects (about_objects.js)", function() {
-  it("object type", function() {
+describe('About Objects (about_objects.js)', function () {
+  it('object type', function () {
     let emptyObject = {};
     // Какой тип у объектов?
-    expect(FILL_ME_IN).toBe(typeof emptyObject);
+    expect('object').toBe(typeof emptyObject);
   });
 
-  it("object literal notation", function() {
+  it('object literal notation', function () {
     let person = {
-      FILL_ME_IN: FILL_ME_IN,
-      FILL_ME_IN: FILL_ME_IN
+      name: 'Amory Blaine',
+      age: 102,
     };
     // Какое имя у объекта person?
-    expect("Amory Blaine").toBe(person.name);
+    expect('Amory Blaine').toBe(person.name);
     // Какое значение в свойстве age?
     expect(102).toBe(person.age);
   });
 
-  it("dynamically adding properties", function() {
+  it('dynamically adding properties', function () {
     let person = {};
-    person.FILL_ME_IN = "Amory Blaine";
-    person.FILL_ME_IN = 102;
+    person.name = 'Amory Blaine';
+    person.age = 102;
     // Какое имя у объекта person?
-    expect("Amory Blaine").toBe(person.name);
-    // Какое значение в свойстве age?
-    expect(102).toBe(person.age);
-  }); 
-
-  it("adding properties from strings", function() {
-    let person = {};
-    person["FILL_ME_IN"] = "Amory Blaine";
-    person["FILL_ME_IN"] = 102;
-    // Какое имя у объекта person?
-    expect("Amory Blaine").toBe(person.name);
+    expect('Amory Blaine').toBe(person.name);
     // Какое значение в свойстве age?
     expect(102).toBe(person.age);
   });
 
-  it("adding functions", function() {
+  it('adding properties from strings', function () {
+    let person = {};
+    person['name'] = 'Amory Blaine';
+    person['age'] = 102;
+    // Какое имя у объекта person?
+    expect('Amory Blaine').toBe(person.name);
+    // Какое значение в свойстве age?
+    expect(102).toBe(person.age);
+  });
+
+  it('adding functions', function () {
     let person = {
-      name: "Amory Blaine",
+      name: 'Amory Blaine',
       age: 102,
-      toString: function() {
-        return FILL_ME_IN;  // Подсказка: используйте ключевое слово 'this' для обращения к объекту person.
+      toString: function () {
+        return `I ${this.name} am ${this.age} years old.`;  // Подсказка: используйте ключевое слово 'this' для обращения к объекту person.
       }
     };
     // Какое значение возвращает метод toString?
-    expect("I Amory Blaine am 102 years old.").toBe(person.toString());
+    expect('I Amory Blaine am 102 years old.').toBe(person.toString());
   });
 
-  it("property enumeration", function() {
+  it('property enumeration', function () {
     let keys = [];
     let values = [];
-    let person = {name: 'Amory Blaine', age: 102, unemployed: true};
-    for(let propertyName in person) {
+    let person = { name: 'Amory Blaine', age: 102, unemployed: true };
+    for (let propertyName in person) {
       keys.push(propertyName);
       values.push(person[propertyName]);
     }
     // Какие имена свойств у объекта?
-    expect(keys).toEqual([ FILL_ME_IN, FILL_ME_IN, FILL_ME_IN]);
+    expect(keys).toEqual(['name', 'age', 'unemployed']);
     // Какие значения свойств у объекта?
-    expect(values).toEqual([ FILL_ME_IN, FILL_ME_IN, FILL_ME_IN]);
+    expect(values).toEqual(['Amory Blaine', 102, true]);
   });
 });
--- JS-Koans/topics/2 Массивы и объекты/about_this.js
+++ JS-Koans/topics/2 Массивы и объекты/about_this.js
@@ -1,47 +1,47 @@
-describe("About this (about_this.js)", function() {
-  it("'this' inside a method", function () {
+describe('About this (about_this.js)', function () {
+  it('"this" inside a method', function () {
     let person = {
       name: 'bob',
       intro: function () {
-        return "Hello, my name is " + this.FILL_ME_IN;
+        return 'Hello, my name is ' + this.name;
       }
-    }
+    };
 
     // Если у объекта есть метод, можно ли получить доступ к свойствам внутри него?
-    expect(person.intro()).toBe("Hello, my name is bob");
+    expect(person.intro()).toBe('Hello, my name is bob');
   });
 
-  it("'this' on unattached function", function () {
+  it('"this" on unattached function', function () {
     let person = {
       globalName: 'bob',
       intro: function () {
-        return "Hello, my name is " + this.globalName;
+        return 'Hello, my name is ' + this.globalName;
       }
-    }
+    };
 
     let alias = person.intro;
 
     // Если функция не вызывается как свойство объекта, 'this' является глобальным контекстом
     // Например глобальный объект window. Пожалуйста, не делайте этого на практике.
-    window.FILL_ME_IN = 'Peter';
+    window.globalName = 'Peter';
 
     // Какое значение будет у this, если это не часть объекта?
-    expect(alias()).toBe("Hello, my name is Peter");
+    expect(alias()).toBe('Hello, my name is Peter');
   });
 
-  it("'this' set explicitly", function () {
+  it('"this" set explicitly', function () {
     let person = {
       name: 'bob',
       intro: function () {
-        return "Hello, my name is " + this.name;
+        return 'Hello, my name is ' + this.name;
       }
-    }
+    };
 
     // Вызов функции с помощью "call" позволяет нам явно назначить "this"
-    let message = person.intro.call({FILL_ME_IN: "Frank"});
+    let message = person.intro.call({ name: 'Frank' });
 
     // К чему относится "this" при использовании метода "call()"?
-    expect(message).toBe("Hello, my name is Frank");
+    expect(message).toBe('Hello, my name is Frank');
   });
 });
 
