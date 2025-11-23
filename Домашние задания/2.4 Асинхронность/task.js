ef088664d911f5cf9440172c0e385af86146fc56
Yaroslav Glivenko <yapridumalemail@gmail.com>
2025-11-23 16:42:20 +0300

    complete 2.4 task

---
+++ Домашние задания/2.4 Асинхронность/task.js
@@ -0,0 +1,68 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) {
+      throw new Error('Отсутствуют обязательные аргументы');
+    }
+
+    if (this.alarmCollection.some(alarm => alarm.time === time)) {
+      console.warn('Уже присутствует звонок на это же время');
+    }
+
+    this.alarmCollection.push({
+      callback,
+      time,
+      canCall: true
+    });
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+  }
+
+  getCurrentFormattedTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
+
+  start() {
+    if (this.intervalId) {
+      return;
+    }
+
+    this.intervalId = setInterval(() => {
+      const currentTime = this.getCurrentFormattedTime();
+
+      this.alarmCollection.forEach(alarm => {
+        if (alarm.time === currentTime && alarm.canCall) {
+          alarm.canCall = false;
+          alarm.callback();
+        }
+      });
+    }, 1000);
+  }
+
+  stop() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach(alarm => {
+      alarm.canCall = true;
+    });
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
